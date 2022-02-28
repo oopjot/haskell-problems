18de1f3b65bafe4c7bf04afa85997b51d8a927cf
oopjot <pitercpo@gmail.com>
2022-02-28 17:41:53 +0100

    48

--- logic_and_codes.hs
+++ logic_and_codes.hs
@@ -1,3 +1,5 @@
+import Control.Monad (replicateM)
+
 -- Problem 46 and 47
 not' :: Bool -> Bool
 not' True = False
@@ -33,4 +35,20 @@ equ' _ _ = False
 
 table :: (Bool -> Bool -> Bool) -> IO ()
 table expr = mapM_ putStrLn [show p ++ "\t" ++ show q ++ "\t" ++ show (expr p q) | p <- [False, True], q <- [False, True]]
+
+
+infixl 4 `or'`
+infixl 4 `nor'`
+infixl 5 `xor'`
+infixl 6 `and'`
+infixl 6 `nand'`
+infixl 3 `equ'`
+
+-- Problem 48
+tablen :: Int -> ([Bool] -> Bool) -> IO ()
+tablen n expr = mapM_ putStrLn [
+    argStr ++ "=> " ++ show  (expr args)
+    | args <- replicateM n [True, False]
+    , let argStr = concatMap ((++ "\t") . show) args
+    ]
 
